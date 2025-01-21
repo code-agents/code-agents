e9a76c816d16244915603a1c1d14414709d959f4
Gunther Brunner <gunta85@gmail.com>
2025-01-21 11:19:13 +0900

    FC

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,13 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import cloudflare from '@astrojs/cloudflare';
+import fulldev from 'fulldev-ui/integration'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  integrations: [react(), /*fulldev(),*/ tailwind({
+    applyBaseStyles: false,
+  })],
   // Configure other settings as needed
   site: 'https://code-agents.com',
   adapter: cloudflare()

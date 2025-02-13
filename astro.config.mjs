758c500a07f05c600ebac1e04871ec336fa281bf
Gunther Brunner <gunta85@gmail.com>
2025-02-14 03:07:47 +0900

    Fix build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,24 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import cloudflare from '@astrojs/cloudflare';
-import fulldev from 'fulldev-ui/integration'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), /*fulldev(),*/ tailwind({
-    applyBaseStyles: false,
-  })],
-  // Configure other settings as needed
+  integrations: [
+    react(),
+    tailwind({
+      applyBaseStyles: false,
+    })
+  ],
   site: 'https://code-agents.com',
-  adapter: cloudflare()
+  adapter: cloudflare(),
+  vite: {
+    resolve: {
+      alias: {
+        '@': '/src'
+      }
+    }
+  }
 });
